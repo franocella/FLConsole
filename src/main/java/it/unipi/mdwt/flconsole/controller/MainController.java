324376bbe90f4ffe45417e208449ed48e522a9cc
winternox <101218223+franocella@users.noreply.github.com>
2024-03-01 12:52:39 +0100

    new Config Controller method STUB

--- src/main/java/it/unipi/mdwt/flconsole/controller/MainController.java
+++ src/main/java/it/unipi/mdwt/flconsole/controller/MainController.java
@@ -4,10 +4,14 @@ import it.unipi.mdwt.flconsole.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -45,4 +49,30 @@ public class MainController {
     public String home() {
         return "main";
     }
+
+    @GetMapping("/newConfig")
+    public ResponseEntity<?> newConfig() {
+    /*
+    try {
+        // TODO: Call the creation Config service
+        // Simulating success
+        boolean success = true;
+        if (success) {
+            // If the new configuration is successful, fetch user configurations
+            List<String> userConfigurations = expConfigService.getUsersConfigList();
+            return ResponseEntity.ok(userConfigurations);
+        } else {
+            // If the new configuration fails, return an error message
+            String errorMessage = "Error during new configuration";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    } catch (Exception e) {
+        // If an exception occurs during the process, return a server error message
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    }*/
+        return null;
+    }
+
+
 }
+
---
+++ src/main/java/it/unipi/mdwt/flconsole/model/ExpConfig.java
@@ -0,0 +1,4 @@
+package it.unipi.mdwt.flconsole.model;
+
+public class ExpConfig {
+}
