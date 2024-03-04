9cddac1ad837dea7d9e45a145db1503ff5a15217
Flavio Messina <messinaflavio00@gmail.com>
2024-03-04 13:17:59 +0100

    Controller and ExpConfigService updates; ExpConfig model;

---
+++ src/main/java/it/unipi/mdwt/flconsole/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package it.unipi.mdwt.flconsole.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+    //TODO: implement create method
+
+    //TODO: implement read method
+
+    //TODO: implement update method
+
+    //TODO: implement delete method
+}
